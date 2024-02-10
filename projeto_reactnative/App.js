416b9f520c5cacd08a0571934bce55c5ca4b7e46
dudaolidev <eduardaoliveira2604@hotmail.com>
2024-02-09 22:40:19 -0300

    Meu primeiro botao

--- projeto_reactnative/App.js
+++ projeto_reactnative/App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <Button
+        title="Press me"
+        onPress={() => Alert.alert('Simple Button pressed')}
+      />
+
     </View>
   );
 }
